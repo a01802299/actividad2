46fa6bd6c21d7323d07d638570448112dffeede9
Diana <a01802299@tec.mx>
2025-10-30 12:42:36 -0600

    Cambios en los colores, no hay rojo para seguir la instrucción

--- snake.py
+++ snake.py
@@ -8,10 +8,15 @@ Exercises
 4. Change the snake to respond to mouse clicks.
 """
 
-from random import randrange
 from turtle import *
-
+from random import randrange, choice
 from freegames import square, vector
+
+
+colores = ['black', 'blue', 'green', 'purple', 'orange']
+
+snake_color= choice(colores)
+food_color = choice([c for c in colores if c!= snake_color])
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
@@ -51,9 +56,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9,snake_color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
     ontimer(move, 100)
 
